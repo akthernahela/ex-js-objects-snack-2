7719094d163b497452c4343d34535e8de621b8b7
akthernahela <nahela00@gmail.com>
2026-04-10 22:56:57 +0200

    The second snack

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //Code Question 1
+/*
 const hamburger = { name: "Cheese Burger", weight: 250 };
 const secondBurger = hamburger;
 secondBurger.name = 'Double Cheese Burger';
@@ -6,7 +7,25 @@ secondBurger.weight = 500;
 
 console.log(hamburger.name); // 'Double Cheese Burger'
 console.log(secondBurger.name); // 'Double Cheese Burger'
-
+*/
 //Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
 //Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
 // Solo un oggetto è stato creato.
+
+
+//Code Question 2
+
+const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+};
+
+const secondBurger = { ...hamburger };
+secondBurger.ingredients[0] = "Salad";
+
+console.log(hamburger.ingredients[0]); // "Salad"
+console.log(secondBurger.ingredients[0]); // "Salad"
+
+//Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+//Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
