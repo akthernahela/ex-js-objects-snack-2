7471367ac545f8c912ffbcba9985829887bbe9b5
akthernahela <nahela00@gmail.com>
2026-04-10 23:02:24 +0200

    Code Question 3

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ console.log(secondBurger.name); // 'Double Cheese Burger'
 
 
 //Code Question 2
-
+/*
 const hamburger = {
     name: "Cheese Burger",
     weight: 250,
@@ -26,7 +26,29 @@ secondBurger.ingredients[0] = "Salad";
 
 console.log(hamburger.ingredients[0]); // "Salad"
 console.log(secondBurger.ingredients[0]); // "Salad"
-
+*/
 //Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
 //Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
 //Sono stati creati due oggetti
+
+
+//Code Question 3
+
+const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    maker: {
+        name: "Anonymous Chef",
+        restaurant: {
+            name: "Hyur's Burgers",
+            address: "Main Street, 123",
+            isOpen: true,
+        },
+        age: 29
+    }
+};
+
+const secondBurger = structuredClone(hamburger);
+const thirdBurger = structuredClone(hamburger);
+
+//Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
