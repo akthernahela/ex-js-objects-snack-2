b40f7d894cdbc96ae6d7ae0e2acb054294f0d578
akthernahela <nahela00@gmail.com>
2026-04-10 22:51:36 +0200

    Add the log result without verify

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const secondBurger = hamburger;
 secondBurger.name = 'Double Cheese Burger';
 secondBurger.weight = 500;
 
-console.log(hamburger.name); // ?
-console.log(secondBurger.name); // ?
+console.log(hamburger.name); // 'Double Cheese Burger'
+console.log(secondBurger.name); // 'Double Cheese Burger'
 
 //Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
 //Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
