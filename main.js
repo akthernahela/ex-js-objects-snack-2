6f2dbe2533a70b7874963542f35dd320410ef68d
akthernahela <nahela00@gmail.com>
2026-04-10 22:48:56 +0200

    Add the first snack

---
+++ main.js
@@ -0,0 +1,4 @@
+//Code Question 1
+
+//Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+//Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
